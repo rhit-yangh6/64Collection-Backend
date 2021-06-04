f45058b0ce782bf6a26274fe6396a46548c82b46
yanghanyu.yangh6 <yanghanyu.yangh6@bytedance.com>
2021-06-04 14:23:06 +0800

    Cors config

---
+++ src/main/java/com/example/backend_64collection/config/CustomizedConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.backend_64collection.config;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@Slf4j
+public class CustomizedConfiguration{
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
+}
